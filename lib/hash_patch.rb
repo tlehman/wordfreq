8f1a86b8fa158eaa0b332d01f4f548cf5557bef8
Tobi Lehman <tobi.lehman@gmail.com>
2012-12-10 22:52:01 -0800

    Remove hash monkey patch and use write instead of puts

--- lib/hash_patch.rb
+++
@@ -1,8 +0,0 @@
-class Hash
-  def to_table
-    columns = self.sort_by { |k,v| -v.to_i }
-    max_len = columns.map { |p| p.first.length }.max
-    width = max_len + 1
-    return columns.map { |p| p.first.ljust(width) + p.last.to_s }.join("\n")
-  end
-end
